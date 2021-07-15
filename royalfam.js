fd753004ac9c1877020ee278bcba922b52905948
Amber Taal <amber.taal@gmail.com>
2021-07-15 17:21:09 +0200

    Objects combined

--- royalfam.js
+++ royalfam.js
@@ -1,21 +1,56 @@
-// Declare a variable 'employer' and assign it an object.
+/*
+Codaisseur Exercise: Hired by one member of the royal family
+As a royal, addressing your people is easy. However, as I assume most of us are not, 
+it might be very useful to create a program to figure out how to address royalty, 
+in case they ever ask us to create their personal website... You never know!
+*/
 
-const employer = {
-    first_name: 'Charles',
-    last_name: 'Peterson',
-    title: 'Baron',
-    gender: 'male'
-}
+const employers = [
+    {
+        first_name: 'Charles',
+        last_name: 'Peterson',
+        title: 'Baron',
+        gender: 'male'
+    },
 
-// Log the properties of your employer-object in your console to check whether you did not make any mistakes.
+    {
+        first_name: 'Erik',
+        last_name: 'The third',
+        title: 'King',
+        gender: 'male'
+    },
+    {
+        first_name: 'Amber',
+        last_name: 'Taal',
+        title: 'Baroness',
+        gender: 'female'
+    }
+]
 
-console.log(Object.values(employer));
-// expeted output: Array [ 'Charles', 'Peterson', 'Baron', 'male' ]
-
-// Anonymous function
+// Declare a variable 'findFormOfAddress' and assign it an anonymous function
 // An anonymous function is a function without a name. An anonymous function is often not accessible after its initial creation.
 
+const findFormOfAddress = function (employer) {
+    if (employer.title === 'King' || employer.title === 'Queen') {
+        console.log('Your Majesty')
+    } else if (employer.title === 'Prince' || employer.title === 'Princess') {
+        console.log('Your Royal Highness')
+    } else if (employer.title === 'Duke' || employer.title === 'Duchess') {
+        console.log('Your Grace')
+    } else if (employer.title === 'Baronet' || employer.title === 'Knight') {
+        console.log(`Sir ${employer.first_name}`)
+    } else if (employer.title === 'Dame') {
+        console.log(`Dame ${employer.first_name}`)
+        // console.log('It is a Marquess/Marchioness/Earl/Countess/Viscount/Viscountess/Baron/Baroness')
+    } else {
+        if (employer.gender === 'male') {
+            console.log(`Lord ${employer.last_name}`)
+        } else if (employer.gender === 'female') {
+            console.log(`Lady ${employer.last_name}`)
+        }
+    }
+}
 
-let findFormOfAddress = (function () {
-    console.log(employer);
-})();
+employers.forEach(function (employer) {
+    findFormOfAddress(employer)
+})
